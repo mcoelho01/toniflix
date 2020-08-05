def653439a0fe918dca81b5f340b2adb2ccfbdd8
Marco Antonio Alves Coelho <malves@MacBook-Pro.local>
2020-08-05 17:42:40 -0300

    tonyflix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,1 @@
-function ButtonLink(props) {
-    return (
-      <a href={props.href} className={props.className}>
-        Thiago 
-      </a> 
-      );
-    }
   
-    export default ButtonLink; 
